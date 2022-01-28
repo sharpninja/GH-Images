5a11a34c26bca903563258a7252110acb7d9cc6a
The Sharp Ninja <ninja@thesharp.ninja>
2022-01-27 23:08:30 -0600

    It works!

--- Upload-Image.ps1
+++ Upload-Image.ps1
@@ -1,15 +1,27 @@
 param(
-    [string]$imagePath, 
-    [string]$message = 'Commited by GH-Images', 
-    [string]$localPath = '~/.gh-images'
+    [string]$imagePath,
+    [string]$message = 'Commited by GH-Images',
+    [string]$localPath = "$env:USERPROFILE\gh-images-repository"
 )
 
 Push-Location
 try {
     if (Test-Path $imagePath) {
         if (-not (Test-Path $localPath)) {
-            gh repo create gh-images-repository --public
-            gh repo clone gh-images-repository $localpath
+            $path = split-path $localPath
+            $repo = split-path $localPath -leaf
+            if(-not (Test-Path $path)){
+                mkdir $path
+            }
+            Set-Location $path
+            $existingRepo = gh repo list --public --json name -q '.[].name' `
+                | Where-Object { $_ -eq $repo }
+
+            if(-not $existingRepo) {
+                gh repo create $repo --public
+            }
+
+            gh repo clone $repo $localpath
         }
 
         Set-Location $localPath
@@ -20,10 +32,12 @@ try {
             git commit -m $message
             git push
 
-            $url=gh browse -n "$newItem"
+            $url=gh browse -n
+            # username/repo[/subdir][@ref]
+            $url = $url.Replace('github.com', 'raw.githubusercontent.com');
         }
 
-        return $url;
+        return "$url/main/$($newItem.Replace(" ", "%20"))"
     }
 }
 finally {
